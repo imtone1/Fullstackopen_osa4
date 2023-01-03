57343a36445d91227da3da7906583bf4675bf7d9
imtone1 <88165529+imtone1@users.noreply.github.com>
2023-01-03 16:08:18 +0200

    osa4 4.19 post token

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs= await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,9 +19,25 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  //token handler
+  const token = getTokenFrom(request)
+  //decodedToken olion sisällä on username ja id
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id){
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     title: body.title,
     author: body.author,
